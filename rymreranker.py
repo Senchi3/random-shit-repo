a41fee215f7ad8496556d3d2fa580b42c2c5b84e
emiliarepo <emiliarepo@icloud.com>
2021-08-27 14:42:51 +0300

    h

--- rymreranker.py
+++ rymreranker.py
@@ -1,70 +1,57 @@
-import re
+import itertools
+import random
+
 
 with open("data/rym.txt", 'r', encoding="utf-8") as infile, \
      open("data/output.txt", 'w', encoding="utf-8") as outfile, \
      open("data/filter.txt", 'r', encoding="utf-8") as filterfile:
-    
+
     # Data preparation
 
     filters = filterfile.readlines()
+    filters = [line.rstrip() for line in filters] # strip newlines for filtering
+
     lines = infile.readlines()
-    data = [[]]
-    count = 0
-    newlines = []
+    lines = [line.rstrip() for line in lines]
+
+    albums = []
 
     # Data filter
-
-    for line in lines:
-        for filter in filters:
-            if filter in line:
-                break
-            else:
-                count =+ 1
-                if count <= len(filters):
-                    newlines.append(line)
-                    break
-
-
-
-    print("HERE ARE THE NEWLINES")
-    print(newlines)
-    print("NO MORE NEWLINES")
+    new_lines = filter(lambda line: not any(filter in line for filter in filters), lines)
 
     # Data formatting
 
-    for line in newlines:
-        # RYM Album, First Name,Last Name,First Name localized, Last Name localized,Title,Release_Date,Rating,Ownership,Purchase Date,Media Type,Review
-        count += 1
-        print("Line #{} was: {}".format(count, line))
-        line = line.replace("\"", "")
-        print("Line #{} is now: {}".format(count, line))
-        line = line.split(",")
-        currentdata = "{} {} {} {}  - {} ({})".format(line[1],line[2],line[3],line[4],line[5],line[6])
-        print("Line #{} has been formatted as: {}".format(count, currentdata))
-        data.append([currentdata, 0])
-    
-    n = len(data)
+    for line in new_lines:
+        line_elements = line.replace('"', '').split(',')
+        formatted_line = "{} {} {} {}  - {} ({})".format(line_elements[1],
+                                                         line_elements[2],
+                                                         line_elements[3],
+                                                         line_elements[4],
+                                                         line_elements[5],
+                                                         line_elements[6])
+        albums.append({
+            'title': formatted_line,
+            'score': 0
+        })
 
+    # gathers all unique album-album combos. Amount of combinations grows extremely quickly
+    # but there's no better way to find, say, the top 10 albums.
+    combinations = list(itertools.combinations(albums, 2))
+    random.shuffle(combinations)
 
-    # NOTE: FROM HERE ON OUT BASICALLY EVERYTHING IS BROKEN
+    for combination in combinations:
+        if combination[0]['title'] == combination[1]['title']:
+            continue
+        print('')
+        print('1. ', combination[0]['title'])
+        print('2. ', combination[1]['title'])
+        answer = input('Which album do you prefer? ')
+        if answer == '1':
+            albums[albums.index(combination[0])].update({'score': combination[0]['score'] + 1})
+        elif answer == '2':
+            albums[albums.index(combination[1])].update({'score': combination[1]['score'] + 1})
+        else:
+            print('Skipping...')
 
-    # Traverse through all array elements
-    for i in range(n-1):
-    # range(n) also work but outer loop will repeat one time more than needed.
-  
-        # Last i elements are already in place
-        for j in range(0, n-i-2):
-  
-            # traverse the array from 0 to n-i-1
-            # Swap if the element found is greater
-            # than the next element
-            # RYM Album, First Name,Last Name,First Name localized, Last Name localized,Title,Release_Date,Rating,Ownership,Purchase Date,Media Type,Review
-            firstoption = data[j + 1]
-            secondoption = data[(j + 2)]
-            print(firstoption)
-            print(secondoption)
-            opinion = int(input("Which one of these is better: (1) {} or (2) {}?".format(firstoption[0], secondoption[0])))
-            if opinion == 1:
-                data[j][1] = opinion
-            elif opinion == 2:
-                data[j + 1][1] = opinion
+    sorted_albums = sorted(albums, key = lambda i: i['score'], reverse = True)
+    print(sorted_albums)
